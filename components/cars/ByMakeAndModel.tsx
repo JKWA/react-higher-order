371caedc5f6a5be7a4fa90a6828f4017d2c6fe27
JKWA <joe.koski@gmail.com>
2022-05-17 09:44:10 -0700

    Initial commit

---
+++ components/cars/ByMakeAndModel.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { getCars, eqMakeAndModel, ordByMake } from '../../data';
+import { sort } from 'fp-ts/lib/Array';
+
+import { withDeduplicateArrayDataProvider, BaseProps } from '../../data-providers';
+
+type Props = BaseProps & { title: string };
+
+const ByMakeAndModelComponent = (props: Props) => {
+  const { title, arrayData = [] } = props;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <ol>
+        {sort(ordByMake)(arrayData).map((item, index) => (
+          <li key={index}>
+            {item.make} {item.model}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default withDeduplicateArrayDataProvider(ByMakeAndModelComponent)(
+  eqMakeAndModel
+)(getCars);
